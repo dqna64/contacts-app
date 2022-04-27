39772365ef8a464bccddafbed7dbc58fabbf37fe
Gordon Huang <dqna64@gmail.com>
2022-04-27 18:26:20 +1000

    fetch and load data

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,38 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import UserCard from './UserCard'
 import logo from './logo.svg'
-import './App.css'
+
+const DATA_URL = 'https://jsonplaceholder.typicode.com/users'
+const DATA_ERROR_MSG = "Sorry, data could not be loaded at this time."
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const [users, setUsers] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+
+  // Fetch and load users on initial render.
+  useEffect(() => {
+    const fetchAndLoadData = async () => {
+      try {
+        const response = await fetch(DATA_URL)
+        const data = await response.json()
+        setUsers(data)
+        console.log(data);
+      } catch (err) {
+        setError(err)
+      }
+      setLoading(false)
+    }
+    fetchAndLoadData()
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div>
+      <div>
+        {loading ? 'Loading...' : error ? DATA_ERROR_MSG : users.map((user) => <UserCard key={user.id} data={user}/>)}
+      </div>
     </div>
   )
 }
