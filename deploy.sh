d491c383edda35133cd0bd02569d5a076621fc80
Gordon Huang <dqna64@gmail.com>
2022-04-28 04:18:06 +1000

    deploy

--- deploy.sh
+++ deploy.sh
@@ -14,8 +14,8 @@ cd dist
 
 # git init
 # git checkout -b main
-# git add -A
-# git commit -m 'deploy'
+git add -A
+git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
