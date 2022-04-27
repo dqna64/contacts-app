4486ac2b2d9cbdaa6d5c04e5f274ca89e47b6f21
Gordon Huang <dqna64@gmail.com>
2022-04-27 23:59:01 +1000

    add loading skeleton ui

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import UserCard from './UserCard'
 import logo from './logo.svg'
 
 const DATA_URL = 'https://jsonplaceholder.typicode.com/users'
-const DATA_ERROR_MSG = "Sorry, data could not be loaded at this time."
+const DATA_ERROR_MSG = 'Sorry, data could not be loaded at this time.'
 
 function App() {
   const [users, setUsers] = useState([])
@@ -17,20 +17,30 @@ function App() {
         const response = await fetch(DATA_URL)
         const data = await response.json()
         setUsers(data)
-        console.log(data);
+        console.log(data)
       } catch (err) {
         setError(err)
       }
       setLoading(false)
     }
     fetchAndLoadData()
-  }, []);
+  }, [])
 
   return (
-    <div className='px-2 py-3 lg:px-12 lg:py-5'>
-      <div className='max-w-screen-sm mx-auto space-y-5 lg:space-y-8'>
-      <div className='text-4xl lg:text-5xl font-bold text-gray-900'>{'My Contacts'}</div>
-        {loading ? 'Loading...' : error ? DATA_ERROR_MSG : users.map((user) => <UserCard key={user.id} data={user}/>)}
+    <div className="px-2 py-3 lg:px-12 lg:py-5">
+      <div className="max-w-screen-sm mx-auto space-y-5 lg:space-y-8">
+        <div className="text-4xl lg:text-5xl font-bold text-gray-900 my-4">{'My Contacts'}</div>
+        {loading ? (
+          <>
+            <UserCard template />
+            <UserCard template />
+            <UserCard template />
+          </>
+        ) : error ? (
+          DATA_ERROR_MSG
+        ) : (
+          users.map((user) => <UserCard key={user.id} data={user} />)
+        )}
       </div>
     </div>
   )
--- src/UserCard.jsx
+++ src/UserCard.jsx
@@ -1,33 +1,90 @@
-import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
-import LinkIcon from '@mui/icons-material/Link';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
-import EmailIcon from '@mui/icons-material/Email';
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone'
+import LinkIcon from '@mui/icons-material/Link'
+import LocationOnIcon from '@mui/icons-material/LocationOn'
+import EmailIcon from '@mui/icons-material/Email'
+import { Skeleton, SkeletonCircle, SkeletonText } from '@chakra-ui/react'
 
-
-function UserCard({data}) {
-    return <div className="border border-gray-400 px-3 lg:px-6 py-3 lg:py-5 bg-slate-100">
-        <div className="text-xl lg:text-2xl font-bold leading-6 lg:leading-6 border-b border-gray-300 pb-[2px] lg:pb-[4px]">{data.name}<span className="block lg:inline text-sm lg:text-base text-gray-500 font-medium lg:ml-2">{"@" + data.username}</span></div>
-        <div className="mt-1 text-xs lg:text-base lg:flex">
-            <div className='font-bold flex items-center grow shrink basis-auto'><EmailIcon sx={{fontSize:14}} /><div className='mx-1'><a href={"mailto:" + data.email}>{data.email}</a></div></div>
-            <div className='font-bold flex items-center grow shrink basis-auto'><LinkIcon sx={{fontSize:14}} /><div className='mx-1'><a href="/">{data.website}</a></div></div>
-            <div className='font-bold flex items-center grow shrink basis-auto'><LocalPhoneIcon sx={{fontSize:14}} /><div className='mx-1'>{data.phone.split(/[-.\s]/).filter((part)=> part[0]!== "x").join(" ")}</div></div>
+function UserCard({ template, data }) {
+  return (
+    <div className="border border-gray-400 px-3 lg:px-6 py-3 lg:py-5 bg-slate-100">
+      <div className="text-xl lg:text-2xl font-bold leading-6 lg:leading-6 border-b border-gray-300 pb-[2px] lg:pb-[4px]">
+        {template ? (<div className='flex items-center'>
+        <SkeletonCircle/><div className='ml-4 grow'><SkeletonText noOfLines={1} /></div>
         </div>
-        <div className='lg:grid lg:grid-cols-2 lg:gap-6 mt-1 lg:mt-2 space-y-1 lg:space-y-0'>
+        ) : (
+          <>
+            <span>{data.name}</span>
+            <span className="block lg:inline text-sm lg:text-base text-gray-500 font-medium lg:ml-2">
+              {'@' + data.username}
+            </span>
+          </>
+        )}
+      </div>
+      <div className="mt-1 text-xs lg:text-base lg:flex">
+        {template ? (
+            <div className='w-full'>
+          <SkeletonText noOfLines={1}/>
+                  
+              </div>
+        ) : (
+          <>
+            <div className="font-bold flex items-center grow shrink basis-auto">
+              <EmailIcon sx={{ fontSize: 14 }} />
+              <div className="mx-1">
+                <a href={'mailto:' + data.email}>{data.email}</a>
+              </div>
+            </div>
+            <div className="font-bold flex items-center grow shrink basis-auto">
+              <LinkIcon sx={{ fontSize: 14 }} />
+              <div className="mx-1">
+                <a href="/">{data.website}</a>
+              </div>
+            </div>
+            <div className="font-bold flex items-center grow shrink basis-auto">
+              <LocalPhoneIcon sx={{ fontSize: 14 }} />
+              <div className="mx-1">
+                {data.phone
+                  .split(/[-.\s]/)
+                  .filter((part) => part[0] !== 'x')
+                  .join(' ')}
+              </div>
+            </div>
+          </>
+        )}
+      </div>
+      <div className="lg:grid lg:grid-cols-2 lg:gap-6 mt-1 lg:mt-2 space-y-1 lg:space-y-0">
         <div className="text-xs lg:text-base">
-            <div className="text-sm lg:text-lg text-slate-500 border-b border-gray-300">Address</div>
-            <p className='font-bold'>{`${data.address.suite} ${data.address.street}, ${data.address.city} ${data.address.zipcode}`}</p>
-
+          <div className="text-sm lg:text-lg text-slate-500 border-b border-gray-300">Address</div>
+          {template ? (
+            <SkeletonText />
+          ) : (
+            <p className="font-bold">{`${data.address.suite} ${data.address.street}, ${data.address.city} ${data.address.zipcode}`}</p>
+          )}
         </div>
         <div className="text-xs lg:text-base">
-            <div className="text-sm lg:text-lg text-slate-500 border-b border-gray-300">Company</div>
-            <p className='font-bold'>{data.company.name}</p>
-            <p className=''>{data.company.catchPhrase}</p>
-            <p className=''>{data.company.bs}</p>
-
+          <div className="text-sm lg:text-lg text-slate-500 border-b border-gray-300">Company</div>
+          {template ? (
+            <SkeletonText />
+          ) : (
+            <>
+              <p className="font-bold">
+                <span
+                  className="hover:cursor-pointer hover:bg-blue-300"
+                  onClick={() => {
+                    navigator.clipboard.writeText(data.company.name)
+                  }}
+                >
+                  {data.company.name}
+                </span>
+              </p>
+              <p className="">{data.company.catchPhrase}</p>
+              <p className="">{data.company.bs}</p>
+            </>
+          )}
         </div>
-
-        </div>
+      </div>
     </div>
+  )
 }
 
 export default UserCard
