f31949f69f62c5c5d4f6d462b4f6ba5e8cc5f1a3
Gordon Huang <dqna64@gmail.com>
2022-04-28 03:38:10 +1000

    scroll to clicked menu item

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import { useEffect, useState } from 'react'
+import { createRef, forwardRef, useEffect, useRef, useState } from 'react'
 import UserCard from './UserCard'
 import logo from './logo.svg'
 import { Spinner } from '@chakra-ui/react'
+import hoverVertStyle from './hoverEffectVert.module.css'
 
 const DATA_URL = 'https://jsonplaceholder.typicode.com/users'
 const DATA_ERROR_MSG = 'Sorry, data could not be loaded at this time.'
@@ -11,14 +12,18 @@ function App() {
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState(null)
 
+  const userRefs = useRef([])
+
   // Fetch and load users on initial render.
   useEffect(() => {
     const fetchAndLoadData = async () => {
       try {
         const response = await fetch(DATA_URL)
-        const data = await response.json()
+        let data = await response.json()
+        userRefs.current = data.map((user, idx) => userRefs.current[idx] ?? createRef())
         setUsers(data)
         console.log(data)
+        console.log(userRefs);
       } catch (err) {
         setError(err)
       }
@@ -27,21 +32,25 @@ function App() {
     fetchAndLoadData()
   }, [])
 
+  useEffect(() => {}, [userRefs.current])
+
+  const scrollToCard = (cardRef) =>{ console.log(cardRef); cardRef.current.scrollIntoView({ behavior: 'smooth' })}
+
   return (
     <div className="px-2 py-3 lg:px-12 lg:py-5">
       <div className="text-4xl lg:text-5xl font-bold text-gray-900 my-4 flex justify-center">
         {'My Contacts'}
       </div>
       <div className="max-w-screen-lg mx-auto flex justify-center items-start sm:space-x-5 lg:space-x-8">
-        <div className="hidden sm:block px-2 lg:px-5 py-3 lg:py-5 grow-0 shrink-0 basis-[180px] lg:basis-[280px] border border-gray-400 bg-slate-100 space-y-2 lg:space-y-3">
+        <div className="hidden sm:block sticky top-4 px-2 lg:px-5 py-3 lg:py-5 grow-0 shrink-0 basis-[180px] lg:basis-[280px] border border-gray-400 bg-slate-100 space-y-1 lg:space-y-2">
           <div className='text-lg lg:text-2xl border-b border-gray-300'>{'Contacts List'}</div>
           {loading ? (
             <Spinner />
           ) : error ? (
             <></>
           ) : (
-            users.map((user) => (
-              <div className="font-semibold text-sm lg:text-xl p-[1px] lg:p-[2px]">{user.name}</div>
+            users.map((user, idx) => (
+              <span className={hoverVertStyle.highlightVert} onClick={() => scrollToCard(userRefs.current[idx])}><span className="font-semibold text-sm lg:text-xl">{user.name}</span></span>
             ))
           )}
         </div>
@@ -55,7 +64,7 @@ function App() {
           ) : error ? (
             DATA_ERROR_MSG
           ) : (
-            users.map((user) => <UserCard key={user.id} data={user} />)
+            users.map((user,idx) => <UserCard key={user.id} reff={userRefs.current[idx]} data={user} />)
           )}
         </div>
       </div>
--- src/UserCard.jsx
+++ src/UserCard.jsx
@@ -3,14 +3,20 @@ import LinkIcon from '@mui/icons-material/Link'
 import LocationOnIcon from '@mui/icons-material/LocationOn'
 import EmailIcon from '@mui/icons-material/Email'
 import { Skeleton, SkeletonCircle, SkeletonText } from '@chakra-ui/react'
+import styles from './hoverEffectHori.module.css'
+import { Tooltip } from '@chakra-ui/react'
 
-function UserCard({ template, data }) {
+function UserCard({ template, data, reff }) {
   return (
-    <div className="border border-gray-400 px-3 lg:px-6 py-3 lg:py-5 bg-slate-100">
+    <div ref={reff} className="border border-gray-400 px-3 lg:px-6 py-3 lg:py-5 bg-slate-100">
       <div className="text-xl lg:text-2xl font-bold leading-6 lg:leading-6 border-b border-gray-300 pb-[2px] lg:pb-[4px]">
-        {template ? (<div className='flex items-center'>
-        <SkeletonCircle/><div className='ml-4 grow'><SkeletonText noOfLines={1} /></div>
-        </div>
+        {template ? (
+          <div className="flex items-center">
+            <SkeletonCircle />
+            <div className="ml-4 grow">
+              <SkeletonText noOfLines={1} />
+            </div>
+          </div>
         ) : (
           <>
             <span>{data.name}</span>
@@ -22,22 +28,25 @@ function UserCard({ template, data }) {
       </div>
       <div className="mt-1 text-xs lg:text-base lg:flex">
         {template ? (
-            <div className='w-full'>
-          <SkeletonText noOfLines={1}/>
-                  
-              </div>
+          <div className="w-full">
+            <SkeletonText noOfLines={1} />
+          </div>
         ) : (
           <>
             <div className="font-bold flex items-center grow shrink basis-auto">
               <EmailIcon sx={{ fontSize: 14 }} />
               <div className="mx-1">
-                <a className='text-blue-500' href={'mailto:' + data.email}>{data.email}</a>
+                <a className="text-blue-500" href={'mailto:' + data.email}>
+                  {data.email}
+                </a>
               </div>
             </div>
             <div className="font-bold flex items-center grow shrink basis-auto">
               <LinkIcon sx={{ fontSize: 14 }} />
               <div className="mx-1">
-                <a className='text-blue-500' href="/">{data.website}</a>
+                <a className="text-blue-500" href="/">
+                  {data.website}
+                </a>
               </div>
             </div>
             <div className="font-bold flex items-center grow shrink basis-auto">
@@ -58,7 +67,20 @@ function UserCard({ template, data }) {
           {template ? (
             <SkeletonText />
           ) : (
-            <p className="font-bold">{`${data.address.suite} ${data.address.street}, ${data.address.city} ${data.address.zipcode}`}</p>
+            <p className="font-bold">
+              <Tooltip label={'Copy to clipboard'}>
+                <span
+                  className={styles.highlight}
+                  onClick={() => {
+                    navigator.clipboard.writeText(
+                      `${data.address.suite} ${data.address.street}, ${data.address.city} ${data.address.zipcode}`,
+                    )
+                  }}
+                >
+                  {`${data.address.suite} ${data.address.street}, ${data.address.city} ${data.address.zipcode}`}
+                </span>
+              </Tooltip>
+            </p>
           )}
         </div>
         <div className="text-xs lg:text-base">
@@ -68,14 +90,16 @@ function UserCard({ template, data }) {
           ) : (
             <>
               <p className="font-bold">
-                <span
-                  className="hover:cursor-pointer hover:bg-blue-300"
-                  onClick={() => {
-                    navigator.clipboard.writeText(data.company.name)
-                  }}
-                >
-                  {data.company.name}
-                </span>
+                <Tooltip label={'Copy to clipboard'}>
+                  <span
+                    className={styles.highlight}
+                    onClick={() => {
+                      navigator.clipboard.writeText(data.company.name)
+                    }}
+                  >
+                    {data.company.name}
+                  </span>
+                </Tooltip>
               </p>
               <p className="">{data.company.catchPhrase}</p>
               <p className="">{data.company.bs}</p>
